571f2c1c646b21578d197fc837df3ec3b4c07443
Anil Kumar <anilkumarsingh0160@gmail.com>
2026-08-04 02:11:22 +0530

    practice on tree questions

--- src/main/java/problem/SizeOfBinaryTree/MainActivity.java
+++ src/main/java/problem/SizeOfBinaryTree/MainActivity.java
@@ -1,5 +1,7 @@
 package problem.SizeOfBinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 /*
 Given the root of a binary tree, return the size of the tree. The size of a binary tree is the total number of nodes in the tree.
@@ -23,6 +25,9 @@ public class MainActivity {
 
         System.out.println("size of binary tree: "+sizeOfTree(treeNode.root));
 
+        System.out.println("level order traversal:");
+        levelOrderTraversal(treeNode.root);
+
     }
 
     public static int sizeOfTree(TreeNode.Node node){
@@ -30,5 +35,30 @@ public class MainActivity {
             return 0;
         return sizeOfTree(node.left) + sizeOfTree(node.right) + 1;
 
+    }
+
+    public static void levelOrderTraversal(TreeNode.Node node) {
+        if (node == null)
+            return;
+
+        Queue<TreeNode.Node> que = new LinkedList<>();
+        que.add(node);
+
+        while (!que.isEmpty()) {
+            int size = que.size();
+
+            while (size-- > 0) {
+                TreeNode.Node temp = que.poll();
+                System.out.print(temp.data + " ");
+
+                if (temp.left != null)
+                    que.add(temp.left);
+
+                if (temp.right != null)
+                    que.add(temp.right);
+            }
+
+            System.out.println();
+        }
     }
 }
